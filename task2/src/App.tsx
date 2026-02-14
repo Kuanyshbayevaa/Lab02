69ce14a62b10319da7de9f6f1e9625aab6486f58
Zangar-Rakhmatulla <rakhmatulla.zangar@mail.ru>
2026-02-14 13:22:32 +0500

    feat: Lab2 Task1 and Task2 completed

---
+++ task2/src/App.tsx
@@ -0,0 +1,12 @@
+import Section from "./Section.jsx";
+import ProductList from "./ProductList.jsx";
+
+function App() {
+  return (
+    <Section title="Product List">
+      <ProductList/>
+    </Section>
+  );
+}
+
+export default App;
